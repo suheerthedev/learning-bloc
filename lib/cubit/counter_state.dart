8a542b08fdd4e2b4baa24c37eeaed1f0036abfaf
suheerthedev <khansuheer1@gmail.com>
2025-09-25 19:41:25 +0500

    Leanred BlocProvider, Listener, Consumer

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -2,6 +2,7 @@ part of 'counter_cubit.dart';
 
 final class CounterState {
   int counterValue;
+  bool wasIncremented;
 
-  CounterState({required this.counterValue});
+  CounterState({required this.counterValue, this.wasIncremented = false});
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learning_bloc/cubit/counter_cubit.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Counter Bloc App',
-      theme: ThemeData(
-        primaryColor: Colors.brown,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Counter Bloc App',
+        theme: ThemeData(
+          primaryColor: Colors.brown,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomePage(title: 'Counter App'),
       ),
-      home: HomePage(title: 'Counter App'),
     );
   }
 }
@@ -31,6 +36,97 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.teal,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Incremented :)'),
+                      duration: Duration(milliseconds: 200),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Decremented :('),
+                      duration: Duration(milliseconds: 200),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'WHOPPS, NEGATIVE VALUE',
+                    style: TextStyle(
+                      fontSize: 26,
+
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  );
+                }
+                if (state.counterValue % 2 == 0) {
+                  return Text(
+                    '${state.counterValue} is Even',
+                    style: TextStyle(
+                      fontSize: 26,
+
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  );
+                } else {
+                  return Text(
+                    '${state.counterValue} is Odd',
+                    style: TextStyle(
+                      fontSize: 26,
+
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  );
+                }
+              },
+            ),
+
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+
+                    // context.bloc<CounterCubit>().decrement();
+                  },
+                  tooltip: 'Decrement',
+                  backgroundColor: Colors.teal,
+                  child: Icon(Icons.remove, color: Colors.white),
+                ),
+
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  backgroundColor: Colors.teal,
+                  child: Icon(Icons.add, color: Colors.white),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
