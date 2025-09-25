20ac262b74b4ede0fd031049fd67b43ce118e23b
suheerthedev <khansuheer1@gmail.com>
2025-09-25 17:26:54 +0500

    Starting basics of Bloc

---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,7 @@
+part of 'counter_cubit.dart';
+
+final class CounterState {
+  int counterValue;
+
+  CounterState({required this.counterValue});
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter Bloc App',
+      theme: ThemeData(
+        primaryColor: Colors.brown,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Counter App'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
