6c6e24edac83ad01a6067dc2a878e6652782d8b3
suheerthedev <khansuheer1@gmail.com>
2025-09-26 15:43:48 +0500

    Created InternetCubit and Setted

---
+++ lib/logic/cubit/internet_cubit.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:equatable/equatable.dart';
+import 'package:learning_bloc/constants/enums.dart';
+
+part 'internet_state.dart';
+
+class InternetCubit extends Cubit<InternetState> {
+  final Connectivity connectivity;
+  late StreamSubscription connectivityStreamSubscription;
+
+  InternetCubit({required this.connectivity}) : super(InternetLoading()) {
+    connectivityStreamSubscription = connectivity.onConnectivityChanged.listen((
+      connectivityResult,
+    ) {
+      if (connectivityResult.isNotEmpty) {
+        if (connectivityResult.first == ConnectivityResult.wifi) {
+          emitInternetConnected(ConnectionType.wifi);
+        } else if (connectivityResult.first == ConnectivityResult.mobile) {
+          emitInternetConnected(ConnectionType.mobileData);
+        } else if (connectivityResult.first == ConnectivityResult.none) {
+          emitInternetDisconnected();
+        }
+      }
+    });
+  }
+
+  void emitInternetConnected(ConnectionType connectionType) =>
+      emit(InternetConnected(connectionType: connectionType));
+
+  void emitInternetDisconnected() => emit(InternetDisconnected());
+
+  @override
+  Future<void> close() {
+    connectivityStreamSubscription.cancel();
+    return super.close();
+  }
+}
