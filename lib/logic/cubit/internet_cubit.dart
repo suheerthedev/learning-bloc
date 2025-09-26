230bf321577312234da15a2df7f958e58836a17a
suheerthedev <khansuheer1@gmail.com>
2025-09-26 13:30:27 +0500

    DOne with InternetCubit

--- lib/logic/cubit/internet_cubit.dart
+++ lib/logic/cubit/internet_cubit.dart
@@ -1,8 +1,38 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:equatable/equatable.dart';
+import 'package:learning_bloc/constants/enums.dart';
 
 part 'internet_state.dart';
 
 class InternetCubit extends Cubit<InternetState> {
-  InternetCubit() : super(InternetInitial());
+  final Connectivity connectivity;
+  late StreamSubscription connectivityStreamSubscription;
+
+  InternetCubit({required this.connectivity}) : super(InternetLoading()) {
+    connectivityStreamSubscription = connectivity.onConnectivityChanged.listen((
+      connectivityResult,
+    ) {
+      if (connectivityResult == ConnectivityResult.wifi) {
+        emitInternetConnected(ConnectionType.wifi);
+      } else if (connectivityResult == ConnectivityResult.mobile) {
+        emitInternetConnected(ConnectionType.mobileData);
+      } else if (connectivityResult == ConnectivityResult.none) {
+        emitInternetDisconnected();
+      }
+    });
+  }
+
+  void emitInternetConnected(ConnectionType connectionType) =>
+      emit(InternetConnected(connectionType: connectionType));
+
+  void emitInternetDisconnected() => emit(InternetDisconnected());
+
+  @override
+  Future<void> close() {
+    connectivityStreamSubscription.cancel();
+    return super.close();
+  }
 }
--- lib/logic/cubit/internet_state.dart
+++ lib/logic/cubit/internet_state.dart
@@ -1,7 +1,5 @@
 part of 'internet_cubit.dart';
 
-
-
 sealed class InternetState extends Equatable {
   const InternetState();
 
@@ -9,4 +7,12 @@ sealed class InternetState extends Equatable {
   List<Object> get props => [];
 }
 
-final class InternetInitial extends InternetState {}
+final class InternetLoading extends InternetState {}
+
+class InternetConnected extends InternetState {
+  final ConnectionType connectionType;
+
+  const InternetConnected({required this.connectionType});
+}
+
+class InternetDisconnected extends InternetState {}
